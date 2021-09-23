dbf7377129dc46efcedfbf21dd26d24f3c47f528
Athos F. Bernardo <80980718+AthosFB@users.noreply.github.com>
2021-09-22 22:23:39 -0300

    noia

--- add/javascript.js
+++ add/javascript.js
@@ -5,13 +5,14 @@ let bune = window.document.getElementsByName("bune") // Boxes Radio
 let laco = window.document.getElementsByName("laco") // Boxes Radio
 let juli = window.document.getElementsByName("juli") // Boxes Radio 
 let fun = window.document.getElementsByName("fun") // Boxes Radio
+let cert = window.document.getElementsByName("cert") // Boxes Radio
 let zoa = window.document.getElementById("zoanoia")
 let noia = " " // Nome Do Nóia
 let fotonoia = " " // Foto Do Nóia
 let sobrenoia = " " // Descrição do Nóia
-let validador = " "
-let cima = 0
-let baixo = 0
+let validador = " " // Pra ver Se Ta Tudo Completado
+let cima = 0 // Quando Preneche O box De Cima
+let baixo = 0 // Quando Preneche O box De Baixo
 function cabo() {
     if (som[0].checked) { // 1
         cima += 1
@@ -62,9 +63,18 @@ function cabo() {
     else {
         validador = "n"
     }
+    if (cert[0].checked) { // 6
+        cima += 1
+    }
+    else if (cert[1].checked) {
+        baixo += 1
+    }
+    else {
+        validador = "n"
+    }
     if (validador != "n") {
         cont.innerHTML = ""
-        resp.innerHTML = `<p>Parabéns Você é Um nóia ${noia}</p><img src="${fotonoia}" alt="Nóia" id="fot"><p>${sobrenoia}</p>`
+        resp.innerHTML = `<p>Parabéns Você é Um nóia ${noia}</p><img src="${fotonoia}" alt="Nóia" id="fot"><p>${sobrenoia} ${cima} -- ${baixo}</p>`
         resp.style.backgroundColor = "#888888"
         resp.style.width = "1200px"
         resp.style.borderRadius = "10px"
@@ -73,7 +83,7 @@ function cabo() {
         fot.style.height = "500px"
     }
     else {
-        zoa.innerText = "Completa os Bagui O Nóia"
+        zoa.innerText = "Completa os Bagui Aí O Nóia"
         zoa.style.font = "normal 30px noia"
         zoa.style.backgroundColor = "#888888"
         zoa.style.padding = "20px"
